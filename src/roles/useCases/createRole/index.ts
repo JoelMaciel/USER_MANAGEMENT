959b3e26abcec95b959165ba553923202f563253
Joel Maciel <soldado25461@gmail.com>
2023-08-08 17:55:08 -0300

    feat: config singleton in RoleRepository

--- src/roles/useCases/createRole/index.ts
+++ src/roles/useCases/createRole/index.ts
@@ -2,7 +2,7 @@ import { RolesRepository } from '../../repositories/RolesRepository';
 import { CreateRolesUseCase } from './CreateRoleUseCase';
 import { CreateRoleController } from './CreateRoleController';
 
-const rolesRepository = new RolesRepository();
+const rolesRepository = RolesRepository.getInstance();
 const createRoleUseCase = new CreateRolesUseCase(rolesRepository);
 export const createRolesController = new CreateRoleController(
   createRoleUseCase,
