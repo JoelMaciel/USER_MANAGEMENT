ad71025f8e127bf75bb82bbf395c2a0d4df7e19f
Joel Maciel <soldado25461@gmail.com>
2023-08-08 00:36:28 -0300

    feat: create findByName method

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -6,14 +6,16 @@ const rolesRepository = new RolesRepository();
 
 rolesRouter.post('/', (request, response) => {
   const { name } = request.body;
+
+  const roleAlreadyExists = rolesRepository.findByName(name);
+
+  if (roleAlreadyExists) {
+    return response.status(400).json({ error: 'Role already exists' });
+  }
+
   const role = rolesRepository.create({ name });
 
   return response.status(201).json(role);
-});
-
-rolesRouter.get('/', (request, response) => {
-  const roles = rolesRepository.findAll();
-  return response.json(roles);
 });
 
 export { rolesRouter };
